6b3f6f60a7e19fb13b68e2aa9026cf1f555a696a
Dmitry Sayfulin <sayfulindmitry@gmail.com>
2024-04-13 01:13:41 +0300

    fix prefix

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import uvicorn
 
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from api.arts_api import get_arts_info_helper
 from api.models import ArtQuery
 
 app = FastAPI()
+api_router = APIRouter(prefix="/api")
 
 # POST: add new exhibit
-@app.post("/add_exhibit/")
+@api_router.post("/add_exhibit/")
 async def add_exhibit(query):
     return
 
-@app.post("/api/get_arts_info/", response_model=list)
+@api_router.post("/get_arts_info/", response_model=list)
 async def get_arts_info(query: ArtQuery):
     
     result = get_arts_info_helper(query)
