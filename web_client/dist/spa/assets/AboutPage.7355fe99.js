cc869ce3af49a5f20d197e30712771b2fde24ab2
Ilya Ivantsov <iivantsov11@gmail.com>
2024-04-14 02:15:32 +0300

    web-client v4-fix

--- web_client/dist/spa/assets/AboutPage.7355fe99.js
+++
@@ -1,1 +0,0 @@
-import{Q as a}from"./QPage.af620ced.js";import{ar as e,ah as t,ak as s,am as c,aj as o}from"./index.14fb0c53.js";const r={},n=o("h1",{class:"text-h6"},"About",-1);function _(f,m){return t(),s(a,{class:"column items-center"},{default:c(()=>[n]),_:1})}var u=e(r,[["render",_]]);export{u as default};
