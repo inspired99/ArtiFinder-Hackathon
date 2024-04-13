b1190bce6099fbbd11b850cca12f9f9464b01a11
Dmitry Sayfulin <sayfulindmitry@gmail.com>
2024-04-13 14:20:48 +0300

    fix api

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ router = APIRouter()
 
 
 @router.get("get_arts_info")
-async def get_arts_info(query: ArtQuery):
+async def get_arts_info():
     return {"message": "Everything is ok"}
 
 @router.post("get_arts_info", response_model=list)
@@ -20,7 +20,11 @@ async def get_arts_info(query: ArtQuery):
     return result
 
 
-app = FastAPI()
+app = FastAPI(
+    docs_url="/api/docs",
+    redoc_url="/api/redoc",
+    openapi_url="/api/openapi.json"
+)
 
 app.include_router(router, prefix="/api")
 
