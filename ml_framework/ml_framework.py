fe84a7bd00607d59b5a45f04ff8191aded1fdd42
Vadim Shabashov <vadim.art.shabashov@gmail.com>
2024-04-14 02:17:13 +0300

    Remove async from ml

--- ml_framework/ml_framework.py
+++ ml_framework/ml_framework.py
@@ -2,50 +2,36 @@ from ml_framework.clip.clip import CLIP
 from ml_framework.resnet.resnet import ResNet
 from ml_framework.llava.llava import Llava
 
-import asyncio
-from concurrent.futures import ProcessPoolExecutor
-
 
 class MLFramework:
-	def __init__(self, executor: ProcessPoolExecutor):
+	def __init__(self):
 		self.clip = CLIP()
 		self.resnet = ResNet(is_train=False)
 		self.llava = Llava()
-		self.executor = executor
 
-	async def get_img_embedding(self, img_path: str):
-		result = await asyncio.get_running_loop().run_in_executor(
-			self.executor, self.clip.get_img_embedding, img_path
-		)
-		return result
+	def get_img_embedding(self, img_path: str):
+		return self.clip.get_img_embedding(img_path)
 
-	async def get_img_class(self, img_path: str):
-		result = await asyncio.get_running_loop().run_in_executor(
-			self.executor, self.resnet.get_img_class, img_path
-		)
-		return result
+	def get_img_class(self, img_path: str):
+		return self.resnet.get_img_class(img_path)
 
-	async def get_img_description(self, img_path: str):
-		result = await asyncio.get_running_loop().run_in_executor(
-			self.executor, self.llava.caption_image, img_path
-		)
-		return result
+	def get_img_description(self, img_path: str):
+		return self.llava.caption_image(img_path)
 
 	def get_emb_size(self) -> int:
 		return self.clip.get_emb_size()
 
 
-async def test_ml():
-	img_path = "~/ArtiFinder/exhibits_database/images/20110231.jpg"
+def test_ml():
+	img_path = "exhibits_database/images/20110231.jpg"
 
-	with ProcessPoolExecutor(max_workers=1) as ml_executor:
-		ml_framework = MLFramework(ml_executor)
+	ml_framework = MLFramework()
 
-		print(ml_framework.get_emb_size())
-		print(await ml_framework.get_img_class(img_path))
-		print(await ml_framework.get_img_embedding(img_path))
-		print(await ml_framework.get_img_description(img_path))
+	print(ml_framework.get_emb_size())
+	print(ml_framework.get_img_class(img_path))
+	print(ml_framework.get_img_embedding(img_path))
+	print(ml_framework.get_img_description(img_path))
 
 
 if __name__ == "__main__":
-	asyncio.run(test_ml())
+	test_ml()
