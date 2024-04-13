a73932b116c3ec8612705f7bcbf8c092d72de638
Ilya Ivantsov <iivantsov11@gmail.com>
2024-04-14 02:02:37 +0300

    web-client v4

--- web_client/dist/spa/assets/AboutPage.a09de3c3.js
+++
@@ -1,1 +0,0 @@
-import{Q as a}from"./QPage.8dbfe2fa.js";import{ah as e,a7 as t,aa as s,ac as c,a9 as o}from"./index.6b5da4f2.js";const r={},n=o("h1",{class:"text-h6"},"About",-1);function _(f,i){return t(),s(a,{class:"column items-center"},{default:c(()=>[n]),_:1})}var u=e(r,[["render",_]]);export{u as default};
---
+++ web_client/dist/spa/assets/pinia.5aeac754.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.14fb0c53.js";import{c as i}from"./pinia.d9eaf920.js";var e=a(({app:o})=>{console.log("pinia boot"),o.use(i())});export{e as default};
