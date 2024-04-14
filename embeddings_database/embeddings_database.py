16eec9d8451cb95461bd58b390ce53eeea1eeee0
Vadim Shabashov <vadim.art.shabashov@gmail.com>
2024-04-14 07:19:34 +0300

    Add save/load in faiss

--- embeddings_database/embeddings_database.py
+++ embeddings_database/embeddings_database.py
@@ -1,11 +1,17 @@
+import os
 import faiss
 import numpy as np
 
 
 class EmbeddingsDatabase:
+	EMBEDDINGS_DATABASE_PATH = "embeddings_database/embeddings_database_idmap.index"
+
 	def __init__(self, emb_size: int):
-		self.index = faiss.IndexFlatL2(emb_size)
-		self.index_idmap = faiss.IndexIDMap(self.index)
+		if os.path.exists(self.EMBEDDINGS_DATABASE_PATH):
+			self.index_idmap = faiss.read_index(self.EMBEDDINGS_DATABASE_PATH)
+		else:
+			self.index = faiss.IndexFlatL2(emb_size)
+			self.index_idmap = faiss.IndexIDMap(self.index)
 
 	def add_embedding(self, img_emb, img_ind):
 		self.index_idmap.add_with_ids(img_emb, img_ind)
@@ -13,3 +19,6 @@ class EmbeddingsDatabase:
 	def find_similar(self, query_emb, k: int):
 		_, indices = self.index_idmap.search(query_emb, k)
 		return indices
+
+	def save_database(self):
+		faiss.write_index(self.index_idmap, self.EMBEDDINGS_DATABASE_PATH)
