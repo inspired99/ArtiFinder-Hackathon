df741f1541683f023b5b3a5dde172c49c2f44e6d
Vadim Shabashov <vadim.art.shabashov@gmail.com>
2024-04-14 10:45:18 +0300

    Fix number

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ async def get_arts_info(query: ArtQuery, limit: int = Query(default=10, ge=1), o
 
         img_path = query.path
         emb = ml_framework.get_img_embedding(img_path)
-        img_ids = embeddings_database.find_similar(emb, k=100)
+        img_ids = embeddings_database.find_similar(emb, k=30)
         logger.info(f"Got ids: {len(img_ids)}")
         with get_db_cursor() as cursor:
             result = await run_db_query(get_arts_info_helper, query, cursor, limit, offset, img_ids)
